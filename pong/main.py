340ef03da0a0021a37d304747f0796ef408dcf32
Joie <jlehman@live.com>
2015-10-12 16:35:04 -0400

    Laid out program foundations

---
+++ pong/main.py
@@ -0,0 +1,32 @@
+#There are 2 paddles, there is one ball
+#if the ball touches the side of the screen of a player
+#the other player gets a point.
+#If the ball hits a paddle it bounces off, maintaining its momentum
+#First to a certain number of points wins
+#Computer AI
+#Strong Separation of concerns
+#main module (game loop)
+#Display module 
+#Input module
+#game logic module 
+    #submodule for number crunching
+#Style guide
+
+display-
+all active rectangles
+scores
+menus
+text stuff
+
+input-
+start input
+up/down
+pause
+
+logic-
+contains squares in play
+
+control-
+make stuff happen
+
+
